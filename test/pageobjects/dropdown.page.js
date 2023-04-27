98e114f3b7a809fdda369d677ba88f79b2065489
MacMini18 <mcm-mac-18@MacMini18s-Mac-mini.local>
2023-04-27 15:00:46 -0700

    initial files

---
+++ test/pageobjects/dropdown.page.js
@@ -0,0 +1,25 @@
+import Page from './page.js';
+
+/**
+ * sub page containing specific selectors and methods for a specific page
+ */
+class dropdownPage extends Page {
+    /**
+     * define selectors using getter methods
+     */
+    get option1 () {
+        return $('//*[@id="dropdown"]/option[2]')
+    }
+    get option2 () {
+        return $('//*[@id="dropdown"]/option[3]')
+    }
+
+    /**
+     * overwrite specific options to adapt it to page object
+     */
+    open () {
+        return super.open('login');
+    }
+}
+
+export default new dropdownPage();
